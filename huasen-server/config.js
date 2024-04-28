46dd8bd89c875aa88294343b8d15bf5065197c69
tomguodong <134027218+tomguodong@users.noreply.github.com>
2024-04-28 10:33:44 +0800

    Update config.js

--- huasen-server/config.js
+++ huasen-server/config.js
@@ -65,8 +65,8 @@ const QQ_MAIL = {
   port: 465,
   secure: true,
   auth: {
-    user: 'QQ邮箱', //  发送方邮箱地址
-    pass: 'QQ邮箱 mtp', //  自己申请的mtp的通行码
+    user: '84826189@qq.com', //  发送方邮箱地址
+    pass: 'iitthljjaxrtbigi', //  自己申请的mtp的通行码
   },
 };
 
